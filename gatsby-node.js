87f3d8fab36245ec1680804ef6df9d1848f0e94a
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2022-01-24 16:57:52 -0800

    auto-generate pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,36 +10,36 @@ const path = require(`path`)
  * Generate pages
  */
 
-// exports.createPages = async ({ actions, graphql, reporter }) => {
-//   const { createPage } = actions
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
 
-//   // Query all the data
-//   const queryResult = await graphql(`
-//     {
-//       pageQuery: allWpPage {
-//         nodes {
-//           databaseId
-//           uri
-//         }
-//       }
-//     }
-//   `)
-//   if (queryResult.errors) {
-//     reporter.panic("error loading events", queryResult.errors)
-//     return
-//   }
+  // Query all the data
+  const queryResult = await graphql(`
+    {
+      pageQuery: allWpPage {
+        nodes {
+          databaseId
+          uri
+        }
+      }
+    }
+  `)
+  if (queryResult.errors) {
+    reporter.panic("error loading events", queryResult.errors)
+    return
+  }
 
-//   // Generate single page pages
-//   const pages = queryResult.data.pageQuery.nodes
-//   pages.forEach(page => {
-//     createPage({
-//       path: page.uri,
-//       component: path.resolve(`./src/templates/page.js`),
-//       context: {
-//         // Data passed to context is available
-//         // in page queries as GraphQL variables.
-//         databaseId: page.databaseId,
-//       },
-//     })
-//   })
-// }
+  // Generate single page pages
+  const pages = queryResult.data.pageQuery.nodes
+  pages.forEach(page => {
+    createPage({
+      path: page.uri,
+      component: path.resolve(`./src/templates/page.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        databaseId: page.databaseId,
+      },
+    })
+  })
+}
