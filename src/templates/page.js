2dd0afedc56d20a167e02dca510b7df76aa7d0a0
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2022-01-25 15:20:01 -0800

    Add new components

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,13 +1,26 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 
+import * as styles from "./page.module.css"
+import Layout from "../components/Layout"
+import Seo from "../components/Seo"
+
 const Page = ({ data }) => {
   const page = data.wpPage
   return (
-    <article>
-      <h1>{page.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: page.content }} />
-    </article>
+    <Layout>
+      <Seo
+        title={page.title}
+        image="/logo.png"
+        pathname={page.uri}
+        // Boolean indicating whether this is an article:
+        article
+      />
+      <article className={styles.article}>
+        <h1>{page.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: page.content }} />
+      </article>
+    </Layout>
   )
 }
 
@@ -18,6 +31,7 @@ export const query = graphql`
     wpPage(databaseId: { eq: $databaseId }) {
       title
       content
+      uri
     }
   }
 `
