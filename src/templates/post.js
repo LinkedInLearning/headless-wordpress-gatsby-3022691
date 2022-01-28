32d332c2fd89010296ecc9503aa4233c597368e8
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2022-01-28 13:27:17 -0800

    Add categories and tags to post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,10 +1,11 @@
 import * as React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { GatsbyImage, getImage, getSrc } from "gatsby-plugin-image"
 
 import * as styles from "./single.module.css"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
+import Catlist from "../components/Catlist"
 
 const Post = ({ data }) => {
   const post = data.currentPost
@@ -29,6 +30,7 @@ const Post = ({ data }) => {
             />
           </figure>
         )}
+        <Catlist postObject={post} />
         <h1 className={styles.article__title}>{post.title}</h1>
         <div className={styles.article__meta}>
           by {post.author.node.name}. Published on{" "}
@@ -42,6 +44,15 @@ const Post = ({ data }) => {
           className={styles.article__content}
           dangerouslySetInnerHTML={{ __html: post.content }}
         />
+        <div>
+          Tagged:{" "}
+          {post.tags.nodes.map((tag, index) => [
+            index > 0 && ", ",
+            <Link key={index} to={tag.link}>
+              {tag.name}
+            </Link>,
+          ])}
+        </div>
       </article>
     </Layout>
   )
@@ -59,6 +70,18 @@ export const query = graphql`
       uri
       author {
         node {
+          name
+        }
+      }
+      categories {
+        nodes {
+          link
+          name
+        }
+      }
+      tags {
+        nodes {
+          link
           name
         }
       }
