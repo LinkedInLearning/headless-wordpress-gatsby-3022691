0f8bf5297a97421413a0e87a9debc155476d463e
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2022-01-28 16:06:46 -0800

    Add tag indexes

--- gatsby-node.js
+++ gatsby-node.js
@@ -49,6 +49,18 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           }
         }
       }
+      tagQuery: allWpTag {
+        nodes {
+          databaseId
+          uri
+          name
+          posts {
+            nodes {
+              databaseId
+            }
+          }
+        }
+      }
     }
   `)
   if (queryResult.errors) {
@@ -107,6 +119,22 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       context: {
         catId: category.databaseId,
         catName: category.name,
+      },
+    })
+  })
+
+  // Create your paginated tag indexes
+  const tags = queryResult.data.tagQuery.nodes
+  tags.map(tag => {
+    paginate({
+      createPage, // The Gatsby `createPage` function
+      items: tag.posts.nodes, // An array of objects
+      itemsPerPage: 4, // How many items you want per page
+      pathPrefix: tag.uri.slice(0, -1), // Creates pages like `/blog`, `/blog/2`, etc
+      component: path.resolve(`./src/templates/tags.js`), // Just like `createPage()`
+      context: {
+        tagId: tag.databaseId,
+        tagName: tag.name,
       },
     })
   })
